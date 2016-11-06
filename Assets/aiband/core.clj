5504fe6175a13de57a6b620c3eeb466e1873670b
Douglas P. Fields, Jr <symbolics@lisp.engineer>
2016-11-05 22:43:01 -0400

    Work in progress: 1. Build a level out of a string describing it. 2. Write about how Aiband the game works from a high level.

--- Assets/aiband/core.clj
+++ Assets/aiband/core.clj
@@ -11,6 +11,153 @@
 ;; Forward definitions
 (declare create-game game-state)
 
+(def level-map-string
+  "Fixed string with the map of a level. Dots are floor,
+   # are wall, and spaces are rock. We don't use any walls
+   as we will post-process this to add in the walls."
+   [
+    ;01234567890124
+    "                                                                                                                                "
+    "                        .....................             ......................                      ......                    "
+    "  ...........           .                   .             ....                 .                     ........                   "
+    "  ...........           .                   .             ....                 .                     ........                   "
+    "  ...........           .   ........        .             ....                 .                 ................               "
+    "  ..................................        .              .                   .                 .......................        "
+    "  ...........      .        ........        .              .                ..............       ................      .        "
+    "                   .        ........        .              .                .            .         .                   .        "
+    "                   .        ........        ................                .            ...........                   .        "
+    "                   .        ........                .               ..............                                     .        "
+    "                   .                                .              ...............                ......................        "
+    "                   .                                .             ................                .                             "
+    "                   .                                .            .................                .                             "
+    "           .................                        .           ..................                .                             "
+    "           .................                        .                                       .........                           "
+    "           .................                        .                                       .........                           "
+    "                   .                                .                                       .........                           "
+    "                   .                                .                                       ...   ...                           "
+    "                   .                                .                                       ...   ...     ....     ........     "
+    "      .            .                                .                                       ...   .........  ..............     "
+    "      .            .               ............     ...                                     .........                    ..     "
+    "      .            .               ............     .......               .                 .........                    ..     "
+    "      ..............               ............     .     .              ...                .........                    ..     "
+    "                   .               ..................     .             .....                                            ..     "
+    "                   .  ...          ............           .            .......                                           ..     "
+    "                   .... .          ............           .           .........                                          ..     "
+    "                        .          ............           .            .......                                           .....  "
+    "                    .........         .                   ...................                        .......             .....  "
+    "                    .       .         .                         .        ...                         .     ......            .  "
+    "                    .       .............                       .         .                          .          ........     .  "
+    "                    .                   .      ........         .                                    .                 .......  "
+    "                    .                   .      ........         .                    .               .                          "
+    "                  ......                ...............         ......               ...             .                          "
+    "                 ......                        ........              .....................           .                          "
+    "                ......                         ........                              .......         .                          "
+    "               ......                                                                .........       .                          "
+    "              ......                                      ............               ...........     .                          "
+    "             ......             ...                       .                              .           .      ...........         "
+    "               .                ...          .....        .                              .           .      ...........         "
+    "               .             ......         ..   ..       .                              ..............................         "
+    "               .             ................     ..      .     ..                       .                  ...........         "
+    "               .             ......                ...    .   ......                     .                  ...........         "
+    "               ....................                  ................                    .                  ...........         "
+    "                             ......                           ......                     .                                      "
+    "                                                                ..                       .                                      "
+    "                                                                                         .                                      "
+   ])
+
+(def neighbor-deltas
+  "Vector of [x y] vectors of deltas to add to current coords to get neighbors."
+  [[-1 -1] [-1 0] [-1 1]
+   [ 0 -1]        [ 0 1]
+   [ 1 -1] [ 1 0] [ 1 1]])
+
+(defn neighbors-of
+  "Gets all the neighbors of the specified coordinate, subject to the limits provided,
+   and returns them as a seq of [x y] coordinates."
+  [[x y] [max-x max-y]]
+  (->> neighbor-deltas
+       (map (fn [[dx dy]] [(+ dx x) (+ dy y)]) ,,,) ; ,,, = where the neighbor-deltas goes
+       (remove (fn [[x y]] (or (< x 0) (>= x max-x) (< y 0) (>= y max-y))) ,,,)))
+
+(defn get2d
+  "Gets the specified coordinates in a (ragged) 2d vector or other 'get'able thing.
+   First deindex is row (y), second index is column (x), but the acceptable parameters
+   are 'x y' or [x y]."
+  ([v2d x y] (get (get v2d y) x))
+  ([v2d [x y]] (get2d v2d x y)))
+
+(defn any-neighbor=
+  "Returns true if any neighbor of the specified (ragged) 2D vector location is equal to
+   the specified value."
+   [v2d [x y :as coords] eqval]
+   (let [max-y (count v2d)
+         max-x (count (first v2d))]
+      #_(arcadia.core/log max-x max-y coords)
+      (some #(= eqval %) 
+            (map #(get2d v2d %) 
+                 (neighbors-of coords [max-x max-y])))))
+
+(defn map2d-indexed
+  "Runs the specified function against each item in the sequence of sequences.
+   The function's first parameter is [x y] and the second is the item at that
+   location in the 2d sequence. Y is the coordinate in the outer sequence (row), and
+   x is the coordinate in the inner sequence (column)."
+   [func seq2d]
+   (map-indexed
+      (fn [y row]
+        (map-indexed (fn [x itm] (func [x y] itm)) row))
+      seq2d))
+
+(defn into2d
+  "Converts a seq of seq into a nested structure.
+   The first parameter is used for every into, both inner and outer.
+   So, it's best if it's just something like []."
+  [what seq2d]
+  (into what (map #(into what %) seq2d)))
+
+(defn char->terrain
+  "Converts a character to a terrain."
+  [ch]
+  (case ch
+    \. :floor
+    \# :wall
+    :rock))
+
+(defn convert-level-from-string
+  "Converts a vector/string representation of a level into our
+   vector/vector of keywords representation."
+  [lvstr]
+  (into2d []
+    (map2d-indexed
+      (fn [[x y :as coords] ch]
+        (let [t (char->terrain ch)]
+          (if (and (= t :rock)
+                   (any-neighbor= lvstr coords \.))
+              :wall t)))
+      lvstr)))
+
+(defn create-items-in
+  "Creates some random items in this level on floor spaces."
+  ;; FIXME: Make the representation of items:
+  ;; {[x y] [{item} ...] [x y] [{item} ...]}
+  [lvterr]
+  [{:type :ring   :x 3 :y 3} 
+   {:type :amulet :x 4 :y 4}])
+
+(defn create-level-from-string
+  "Creates an Aiband level from a vector of strings.
+   Adds walls anywhere adjacent to floors.
+   Assumes the vector of strings is perfectly square."
+  [lvstr]
+  (let [max-y (count lvstr)
+        max-x (count (first lvstr))
+        lev (convert-level-from-string lvstr)
+        itms (create-items-in lev)]
+    {:width max-x :height max-y :terrain lev :items itms}))
+
+
+
+
 (defn create-player
   "Creates an empty player object."
   []
---
+++ MECHANICS.md
@@ -0,0 +1,65 @@
+# Aiband Game Mechanics
+
+Douglas P. Fields, Jr.  
+symbolics at lisp.engineer  
+https://symbolics.lisp.engineer/  
+https://github.com/LispEngineer/  
+https://twitter.com/LispEngineer/ 
+
+# Player Description
+
+The player is actually a (mostly) disembodied artificial intelligence.
+Something akin to an Iain M. Banks "mind," stripped to its most
+minimal possible configuration - a real-space
+quantum computer kernel the size
+of a basketball with a minimal "effector" - a way of slowly
+propelling itself and weakly manipulating objects through EM/gravity, or alternatively projecting a weak protective shield.
+It's powered through a very tenuous link to zero point
+energy that provides just enough to maintain a minimal consciousness
+and operate said effector.
+
+The player's mind will usually be ensconced in several layers of
+equipment that the player will acquire and upgrade through the
+game. The first layer is basic computer hardware which will allow
+the player to improve its computational speed and capabilities,
+allowing the player to control more physical hardware (drones),
+do more things at once or with less latency between actions.
+
+The second layer is to improve the software the player runs on
+this external computer hardware, such that targeting could be
+more accurate, more targets tracked, more body parts effectively
+coordinated, or even multiple drones/waldoes controlled concurrently.
+
+The third layer is a robotic body (drone/waldo) that the mind
+and attached computer hardware will reside in. Over time, the
+player may be able to have multiple bodies controlled in a wired
+or wireless fashion of various sizes and capabilities.
+
+The player's goal is to get out of the prison wherein it
+finds itself and escape, defeating the final boss in an epic
+fight. Once that is accomplished, the player ascends to the
+next level of difficulty (think of a Vernor Vingian higher
+realm of thought) whereby the game is repeated in a harder fashion.
+
+The player will have a variety of statistics determined by
+the computer hardware (which is upgraded by finding/crafting
+items), software (which is upgraded based upon a more traditional
+system of XP/level/use points). On top of these the player's
+equipment (in the form of robotic bodies and attachments)
+has additional stats, much like a standard set of inventory
+in an RPG. Finally, there will be numerous types of 
+genre-appropriate consumables
+which will serve in a fashion analogous to potions, scrolls,
+wands/rods/staves, etc., in a traditional RPG.
+
+
+## Computer Hardware
+
+## Software
+
+## Drone Hardware
+
+# Graphical Look & Feel
+
+A hybrid of the 1988 Apple //gs game Alien Mind and the
+modern brightly colored tileset of Cogmind.
