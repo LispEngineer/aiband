09495e81738cfc8f6039962d584804b9ea1e6095
Douglas P. Fields, Jr <symbolics@lisp.engineer>
2016-11-08 21:48:01 -0500

    Visualiation of the generated BSP

--- Assets/aiband/bsp.clj
+++ Assets/aiband/bsp.clj
@@ -32,12 +32,36 @@
                (if vs (:min-y bi) (+ d-split 1))
                (:max-x bi) (:max-y bi) [])]))
 
+(defn visualize-internal
+  "Recursively fills in the 2d vector of characters with letters representing each
+   node of the BSP."
+  [bsp v node-num]
+  (if (nil? bsp)
+    ;; Terminal case - no bsp
+    [v node-num]
+    ;; Recursive case - a bsp
+    (let [node-char (char (+ (int \A) node-num))
+          v-node (map2d-indexed 
+                  (constantly node-char)
+                  [(:min-x bsp) (:min-y bsp) (:max-x bsp) (:max-y bsp)]
+                  v)
+          [left-bsp right-bsp] (:children bsp)
+          [v-left num-left]   (visualize-internal left-bsp v-node (inc node-num))
+          [v-right num-right] (visualize-internal right-bsp v-left num-left)]
+      [v-right num-right])))
+
 (defn visualize
+  "Turns the BSP into a set of strings that represents all the partitions
+   graphically."
   [bi]
-  (let [w (:max-x bi)
-        h (:max-y bi)
-        v (vec2d w h \.)]
-    v))
+  (let [w (inc (:max-x bi)) ; These maxes are inclusive
+        h (inc (:max-y bi))
+        v (vec2d w h \.)
+        v-seq (first (visualize-internal bi v 0))]
+    (into [] (map #(apply str %) v-seq))))
+
+;; To print the visualization:
+#_(do (doall (map println (visualize (partition-bsp (gen-bsp 90 35))))) nil)
 
 (defn partition-bsp
   "Returns the input bsp with children added, or
